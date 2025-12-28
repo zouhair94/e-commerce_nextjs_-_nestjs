6072962dfade4d0eb0d64570f94514ced47b9dc6
zouhair ridaoui <z.ridaoui94@outlook.com>
2025-12-28 09:16:02 +0300

    feat: integrate ConfigModule and UsersModule into AppModule, add global validation pipe, and update package dependencies

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [],
   providers: [],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(app.get(ConfigService)?.getOrThrow('PORT'));
 }
 bootstrap();
