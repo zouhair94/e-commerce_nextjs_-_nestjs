0c1e132299c8758ba45fc5eedb6fdec4c2c46d6e
zouhair ridaoui <z.ridaoui94@outlook.com>
2026-01-09 00:17:07 +0300

    refactor(prisma): update PrismaService to use PostgreSQL adapter and connection pooling
    - Modified PrismaService to extend PrismaClient and utilize PrismaPg adapter with a connection pool.
    - Removed module lifecycle hooks for connection management.
    - Updated tsconfig.json to disable declaration files.
    - Added @prisma/adapter-pg and pg dependencies in pnpm-lock.yaml.

--- apps/backend/src/prisma/prisma.service.ts
+++ apps/backend/src/prisma/prisma.service.ts
@@ -1,14 +1,13 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
-  async onModuleDestroy() {
-    await this.$disconnect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
   }
 }
