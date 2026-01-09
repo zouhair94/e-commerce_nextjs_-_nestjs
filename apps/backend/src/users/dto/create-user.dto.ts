9f9055077f8da26f330120373fe1cd256e926006
zouhair ridaoui <z.ridaoui94@outlook.com>
2026-01-09 18:32:05 +0300

    fix: add IsString validation to name field in CreateUserDto

--- apps/backend/src/users/dto/create-user.dto.ts
+++ apps/backend/src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
 export class CreateUserDto {
   @IsEmail()
   email: string;
@@ -6,5 +6,6 @@ export class CreateUserDto {
   @IsStrongPassword()
   password: string;
 
+  @IsString()
   name?: string;
 }
