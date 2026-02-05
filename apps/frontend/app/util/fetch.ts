e3f6156e8789c2c90339adfc44c2f2d0114d1337
zouhair ridaoui <z.ridaoui94@outlook.com>
2026-02-06 01:11:47 +0300

    feat: remove debug logging from fetch utility

--- apps/frontend/app/util/fetch.ts
+++ apps/frontend/app/util/fetch.ts
@@ -13,7 +13,6 @@ export const post = async (path: string, data: FormData | object) => {
 
   const resData = await response.json();
 
-  console.log("Response Data:", resData);
   if (resData.error) {
     return { error: getErrorMessage(resData.message) };
   }
