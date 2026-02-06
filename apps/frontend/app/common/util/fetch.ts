9b3354c1c253286fb261f1118418db82fbfd581c
zouhair ridaoui <z.ridaoui94@outlook.com>
2026-02-06 17:53:09 +0300

    feat: add getHeaders utility for cookie management in fetch requests

--- apps/frontend/app/common/util/fetch.ts
+++ apps/frontend/app/common/util/fetch.ts
@@ -1,5 +1,10 @@
 import { API_URL } from "@/app/common/constants/api";
 import { getErrorMessage } from "@/app/common/util/errors";
+import { cookies } from "next/headers";
+
+export const getHeaders = async () => ({
+  Cookie: (await cookies()).toString(),
+});
 
 export const post = async (path: string, data: FormData | object) => {
   const body = data instanceof FormData ? Object.fromEntries(data) : data;
@@ -7,8 +12,28 @@ export const post = async (path: string, data: FormData | object) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      ...(await getHeaders()),
     },
     body: JSON.stringify(body),
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    return { error: getErrorMessage(resData.message) };
+  }
+
+  return { error: null, data: resData };
+};
+
+export const get = async (path: string) => {
+  const cookie = await getHeaders();
+  const response = await fetch(`${API_URL}/${path}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      ...cookie,
+    },
   });
 
   const resData = await response.json();
