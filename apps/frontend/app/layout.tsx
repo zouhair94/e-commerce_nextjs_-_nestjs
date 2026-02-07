31da488a4a1eaa36905713257f2baebbf7ee0a35
zouhair ridaoui <z.ridaoui94@outlook.com>
2026-02-08 02:25:25 +0300

    feat: add Header component for improved navigation and layout

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 import darkTheme from "./dark.theme";
+import Header from "./header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <AppRouterCacheProvider>
           <ThemeProvider theme={darkTheme}>
             <CssBaseline />
+            <Header />
             <Container>
               {children}
             </Container>
