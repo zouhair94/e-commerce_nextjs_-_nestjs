c543464f8c13ff2137f55f142a40f98a1fff029e
zouhair ridaoui <z.ridaoui94@outlook.com>
2026-02-08 02:25:33 +0300

    feat: add routes for authentication and home navigation

---
+++ apps/frontend/app/common/constants/routes.ts
@@ -0,0 +1,5 @@
+export const unAuthenticatedRoutes = [
+  { name: "Login", path: "/auth/login" },
+  { name: "Sign Up", path: "/auth/sign-up" },
+];
+export const routes = [{ name: "Home", path: "/" }];
